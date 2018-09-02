7f6cd6d4798f611fdc324a3fc21ab1aa2b164c28
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-09-02 18:53:27 +0100

    extraitems.lua: [new code] implement proper info readout for debug probe

--- extraitems.lua
+++ extraitems.lua
@@ -7,12 +7,17 @@ Extra items for tinycubes work.
 
 -- a debug probe tool;
 -- prints out info about a tiny cube when used on one.
+local lib = "com.github.thetaepsilon.minetest.libmthelpers"
+local format = mtrequire(lib..".coords").format
+
 local apiname = _mod.entity.apiname
-local msg = "STUB debug probe not implemented"
 local rightclick = function(api, clicker, itemstack)
 	-- stub for now, will use api later when defined
 	if clicker:is_player() then
 		local n = clicker:get_player_name()
+		local cpos = api("get_cpos")
+		local scale = api("get_scale")
+		local msg = "cube: center pos "..format(cpos).." scale "..scale
 		minetest.chat_send_player(n, msg)
 	end
 end
