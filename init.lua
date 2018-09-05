61075a01e982c61896dd492bece639e0a46cc839
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-09-05 22:56:38 +0100

    init.lua: [enhancement] first version of tiny cubes stacking ability

--- init.lua
+++ init.lua
@@ -101,10 +101,53 @@ local on_place = function(itemstack, user, pointed)
 	place_cube_from_surface_dir(pointed.surface, direction, bp)
 end
 
+
+
+-- to use the same technique to place against existing entities,
+-- we need to be a bit more clever here - can't use pointed_thing_to_face_pos().
+-- thankfully vectorextra's cube intersection does the heavy lifting for us;
+-- all we have to do is get the head height.
+local apiname = _mod.entity.apiname
+local m_camera = modtable("ds2.minetest.misc_game_routines.get_camera_pos")
+local get_base_and_eye_pos = m_camera.get_base_and_eye_pos
+local m_solve = mtrequire("ds2.minetest.vectorextras.cube_intersect_solve")
+local solve_ws_raw = m_solve.solve_ws_raw
+local face_offsets = m_solve.enum_offsets
+
+local s = {}
+local stack_cube = function(api, clicker, itemstack)
+	if not clicker:is_player() then return nil end
+	-- variables needed for solving -
+	-- camera pos, look dir, entity pos, cube widths
+	local e = api("get_cpos")
+	local _, c = get_base_and_eye_pos(clicker)
+	local l = clicker:get_look_dir()
+	-- TODO: cuboids like slabs? just assume scale for now
+	local scale = api("get_scale")
+	local w = 1 / sfp(scale)
+
+	-- wow many variable such line length
+	local fx, fy, fz, es, ef =
+		solve_ws_raw(c.x, c.y, c.z, l.x, l.y, l.z, e.x, e.y, e.z, w, w, w)
+
+	-- shouldn't happen really...
+	if fx == nil then
+		prn("NOPE")
+		return
+	end
+	local face = es..ef
+	local dir = face_offsets[face]
+	s.x, s.y, s.z = fx, fy, fz
+	place_cube_from_surface_dir(s, dir, bp)
+end
+
 local item = mn..":tinycubetool"
 minetest.register_craftitem(item, {
 	inventory_image = "tinycubes_tinycubetool.png",
 	on_place = on_use_surface_only(on_place),
+	on_entity_rightclick = {
+		[apiname] = stack_cube,
+	}
 })
 
 
